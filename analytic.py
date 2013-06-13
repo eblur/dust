e0a6f6e22e87dc9e193974a0d70f8c37c3169963
lia <lia.astro@gmail.com>
2013-06-13 16:38:25 -0400

    Analytic functions now process halo dictionaries

--- analytic.py
+++ analytic.py
@@ -6,6 +6,9 @@ from scipy.special import erf
 from scipy.integrate import trapz
 
 ## REQUIRES Halo objects to work
+from halodict import HaloDict
+from halo import Halo
+
 import galhalo as GH
 import dust
 
@@ -28,194 +31,181 @@ from scipy.special import gamma
 from scipy.special import expi
 
 def GammaInc( a, z ):
-	if z.any() < 0:
-		print 'ERROR: z must be >= 0'
-		return
-	if a == 0:
-		return -expi(-z)
-
-	elif a < 0:
-		return ( GammaInc(a+1,z) - np.power(z,a) * np.exp(-z) ) / a
-	else:
-		return gammaincc(a,z) * gamma(a)
-
+    if z.any() < 0:
+        print 'ERROR: z must be >= 0'
+        return
+    if a == 0:
+        return -expi(-z)
+    
+    elif a < 0:
+        return ( GammaInc(a+1,z) - np.power(z,a) * np.exp(-z) ) / a
+    else:
+        return gammaincc(a,z) * gamma(a)
 
 def set_htype( halo, xg=None, NH=1.0e20, d2g=0.009 ):
-	'''
-	Sets galactic ISM htype values for Halo object
+    '''
+    Sets galactic ISM htype values for Halo object
     --------------------------------------------------------------
-	FUNCTION set_htype( halo, xg=None, NH=1.0e20, d2g=0.009 )
-	RETURN : empty
+    FUNCTION set_htype( halo, xg=None, NH=1.0e20, d2g=0.009 )
+    RETURN : empty
     --------------------------------------------------------------
-	halo : halo.Halo object
-	xg  : float [0-1] : Position of screen where 0 = point source, 1 = observer
-		- if None, htype set to 'Uniform'
-		- otherwise, hytype set to 'Screen'
-	NH  : float [cm^-2] : Hydrogen column density
-	d2g : float : Dust-to-gas mass ratio
-	'''
-#	if halo.htype != None:
-#		print 'WARNING: Halo already has an htype. Overwriting now'
+    halo : halo.Halo object
+    xg  : float [0-1] : Position of screen where 0 = point source, 1 = observer
+        - if None, htype set to 'Uniform'
+        - otherwise, hytype set to 'Screen'
+    NH  : float [cm^-2] : Hydrogen column density
+    d2g : float : Dust-to-gas mass ratio
+    '''
+    if halo.htype != None:
+        print 'WARNING: Halo already has an htype. Overwriting now'
 
-	if xg == None:
-		halo.htype = GH.GalHalo( NH=NH, d2g=d2g, ismtype='Uniform' )
-	else:
-		halo.htype = GH.GalHalo( xg=xg, NH=NH, d2g=d2g, ismtype='Screen' )
+    if xg == None:
+        halo.htype = GH.GalHalo( NH=NH, d2g=d2g, ismtype='Uniform' )
+    else:
+        halo.htype = GH.GalHalo( xg=xg, NH=NH, d2g=d2g, ismtype='Screen' )
 
-	md         = NH * GH.c.mp() * d2g
-	halo.dist  = dust.Dustspectrum( rad=halo.rad, md=md )
-	halo.taux  = GH.ss.Kappascat( E=halo.energy, scatm=halo.scatm, dist=halo.dist ).kappa * halo.dist.md
-	return
+    md         = NH * GH.c.mp() * d2g
+    halo.dist  = dust.Dustspectrum( rad=halo.rad, md=md )
+    halo.taux  = GH.ss.Kappascat( E=halo.energy, scatm=halo.scatm, dist=halo.dist ).kappa * halo.dist.md
+    return
 
 #--------------------------------------------
 ## Screen case ISM
 
 def G_p( halo ):
-	'''
-	Returns integral_a0^a1 a^(4-p) da
-	'''
-	a0 = halo.dist.a[0]
-	a1 = halo.dist.a[-1]
-	p  = halo.rad.p
-	if p == 5:
-		return np.log( a1/a0 )
-	else:
-		return 1.0/(5.0-p) * ( np.power(a1,5.0-p) - np.power(a0,5.0-p) )
+    '''
+    Returns integral_a0^a1 a^(4-p) da
+    '''
+    a0 = halo.dist.a[0]
+    a1 = halo.dist.a[-1]
+    p  = halo.rad.p
+    if p == 5:
+        return np.log( a1/a0 )
+    else:
+        return 1.0/(5.0-p) * ( np.power(a1,5.0-p) - np.power(a0,5.0-p) )
 
 def G_s( halo ):
-	'''
-	Function used for evaluating halo from power law distribution of grain sizes (Screen case)
-	'''
-	a0 = halo.dist.a[0]
-	a1 = halo.dist.a[-1]
-	p  = halo.rad.p
-	charsig0 = 1.04 * 60.0 / halo.energy
-	pfrac    = (7.0-p)/2.0
-	const    = halo.alpha**2/(2.0*charsig0**2*halo.htype.xg**2)
-	gamma1   = GammaInc( pfrac, const * a1**2 )
-	gamma0   = GammaInc( pfrac, const * a0**2 )
-	return -0.5 * np.power( const, -pfrac ) * ( gamma1 - gamma0 )
+    '''
+    Function used for evaluating halo from power law distribution of grain sizes (Screen case)
+    '''
+    a0 = halo.dist.a[0]
+    a1 = halo.dist.a[-1]
+    p  = halo.rad.p
+    
+    if type(halo) == Halo:
+        energy, alpha = halo.energy, halo.alpha
+    if type(halo) == HaloDict:
+        energy, alpha = halo.superE, halo.superA
+    
+    charsig0 = 1.04 * 60.0 / energy
+    pfrac    = (7.0-p)/2.0
+    const    = alpha**2/(2.0*charsig0**2*halo.htype.xg**2)
+    gamma1   = GammaInc( pfrac, const * a1**2 )
+    gamma0   = GammaInc( pfrac, const * a0**2 )
+    return -0.5 * np.power( const, -pfrac ) * ( gamma1 - gamma0 )
 
-def G_s_dict( halodict ):
-
-	energy = halodict.energy
-	ref    = halodict[ energy[0] ]
-
-	a0 = ref.dist.a[0]
-	a1 = ref.dist.a[-1]
-	p  = ref.rad.p
-	
-	NE = len(energy)
-	NA = len(ref.alpha)
-	superA = np.tile( ref.alpha, (NE,1) ) # NE x NA
-	superE = np.tile( energy.reshape(NE,1), NA )
-	
-	charsig0 = 1.04 * 60.0 / superE
-	pfrac    = (7.0-p)/2.0
-	const    = superA**2 / ( 2.0 * charsig0**2 * ref.htype.xg**2 )
-	gamma1   = GammaInc( pfrac, const * a1**2 ) # note 'a' is always a single value
-	gamma0   = GammaInc( pfrac, const * a0**2 )
-	return -0.5 * np.power( const, -pfrac ) * ( gamma1 - gamma0 )
-
-def screen_eq( halo ):
-	'''
-	Analytic function for a screen of dust particles 
-		from parameters set in halo (taux, a0, a1, p, xg)
+def screen_eq( halo, xg=0.5, **kwargs ):
+    '''
+    Analytic function for a screen of dust particles 
+        from parameters set in halo (taux, a0, a1, p, xg)
     --------------------------------------------------------------
-	FUNCTION screen_eq( halo )
-	RETURNS : np.array [arcsec^-2] : I_h/F_a
-	Also alters halo.intensity with the resulting valuesa
+    FUNCTION screen_eq( halo )
+    RETURNS : np.array [arcsec^-2] : I_h/F_a
+    Also alters halo.intensity with the resulting valuesa
     --------------------------------------------------------------
-	halo : halo.Halo object
-	'''
-	hfrac = halo.taux
-
-	if type(halo.rad) == dust.Grain:
-		charsig = 1.04 * 60. / halo.rad.a  / halo.energy  #arcsec
-		gterm  = np.exp( -halo.alpha**2 / (2 * charsig**2 * halo.htype.xg**2) )
-		result = hfrac * gterm / ( halo.htype.xg**2 * 2.0*np.pi*charsig**2 )
-
-	if type(halo.rad) == dust.Dustdist:
-		charsig0 = 1.04 * 60.0 / halo.energy
-		const = hfrac / ( 2.0*np.pi*charsig0**2 )
-		result = const / halo.htype.xg**2 * G_s(halo) / G_p(halo)
-
-	halo.intensity = result
-	return result
-
-def screen_eq_dict( halodict, xg=0.5, **kwargs):
-
-	energy = halodict.energy # NE x 1
-	ref    = halodict[ energy[0] ] # A reference halo
-	# Assume all other halos have same properties except for energy
-	set_htype( ref, xg=xg, **kwargs )
-	hfrac  = ref.taux * energy[0]**2 / energy**2 # NE x 1
-	
-	NE     = len(halodict.energy)
-	NA     = len(ref.alpha)
-
-	superA = np.tile( ref.alpha, (NE,1) ) # NE x NA
-	superE = np.tile( energy.reshape(NE,1), NA ) # NE x NA
-	supertau = np.tile( hfrac.reshape(NE,1), NA )
-	
-	if type(ref.rad) == dust.Grain:
-		charsig = 1.04 * 60. / ref.rad.a / superE # NE x NA [arcsec]
-		gterm   = np.exp( -superA**2 / (2 * charsig**2 * ref.htype.xg**2) ) # NE x NA
-		result  = supertau * gterm / ( ref.htype.xg**2 * 2.0*np.pi*charsig**2 ) # NE x NA
-	
-	if type(ref.rad) == dust.Dustdist:
-		charsig0 = 1.04 * 60.0 / superE # NE x NA
-		const    = supertau / ( 2.0*np.pi*charsig0**2 ) # NE x NA
-		result   = const / ref.htype.xg**2 * G_s_dict(halodict) / G_p(ref)
-		## Have to fix G_s to take whatever energy
-	
-	halodict.intensity = result
-	return result
+    halo : halo.Halo object
+    '''
+    
+    set_htype( halo, xg=xg, **kwargs )
+    hfrac, energy, alpha = np.array([]), np.array([]), np.array([])
+    
+    if type(halo) == Halo:
+#        print 'Using a Halo object'
+        hfrac = halo.taux
+        energy, alpha = halo.energy, halo.alpha
+    
+    if type(halo) == HaloDict:
+#        print 'Using a halo dictionary'
+        NE, NA = len(halo.energy), len(halo.alpha)
+        hfrac  = np.tile( halo.taux.reshape(NE,1), NA ) # NE x NA
+        energy, alpha = halo.superE, halo.superA # NE x NA
+    
+    if type(halo.rad) == dust.Grain:
+#        print 'Using a dust grain'
+        charsig = 1.04 * 60. / halo.rad.a  / energy  #arcsec
+        gterm  = np.exp( -alpha**2 / (2 * charsig**2 * xg**2) )
+        result = hfrac * gterm / ( xg**2 * 2.0*np.pi*charsig**2 )
+    
+    if type(halo.rad) == dust.Dustdist:
+#        print 'Using a distribution of grain sizes'
+        charsig0 = 1.04 * 60.0 / energy
+        const = hfrac / ( 2.0*np.pi*charsig0**2 )
+        result = const / xg**2 * G_s(halo) / G_p(halo)
+    
+    halo.intensity = result
+    return
 
 #--------------------------------------------
 ## Uniform case ISM
 
 def G_u( halo ):
-	'''
-	Function used for evaluating halo from power law distribution of grain sizes (Uniform case)
-	'''
-	a0 = halo.rad.a[0]
-	a1 = halo.rad.a[-1]
-	p  = halo.rad.p
-	power = 6.0 - halo.rad.p
-	pfrac = (7.0-p) / 2.0
-	charsig = 1.04 * 60.0 / halo.energy
-	const   = halo.alpha / charsig / np.sqrt(2.0)
-	A1 = np.power(a1,power) * ( 1 - erf(const*a1) )
-	A0 = np.power(a0,power) * ( 1 - erf(const*a0) )
-	B1 = np.power(const,-power) * GammaInc( pfrac, const**2 * a1**2 ) / np.sqrt(np.pi)
-	B0 = np.power(const,-power) * GammaInc( pfrac, const**2 * a0**2 ) / np.sqrt(np.pi)
-	return ( (A1-B1) - (A0-B0) ) / power
+    '''
+    Function used for evaluating halo from power law distribution of grain sizes 
+    (Uniform case)
+    '''
+    a0 = halo.rad.a[0]
+    a1 = halo.rad.a[-1]
+    p  = halo.rad.p
+    
+    if type(halo) == Halo:
+        energy, alpha = halo.energy, halo.alpha
+    if type(halo) == HaloDict:
+        energy, alpha = halo.superE, halo.superA
+    
+    power = 6.0 - halo.rad.p
+    pfrac = (7.0-p) / 2.0
+    charsig = 1.04 * 60.0 / energy
+    const   = alpha / charsig / np.sqrt(2.0)
+    
+    A1 = np.power(a1,power) * ( 1 - erf(const*a1) )
+    A0 = np.power(a0,power) * ( 1 - erf(const*a0) )
+    B1 = np.power(const,-power) * GammaInc( pfrac, const**2 * a1**2 ) / np.sqrt(np.pi)
+    B0 = np.power(const,-power) * GammaInc( pfrac, const**2 * a0**2 ) / np.sqrt(np.pi)
+    return ( (A1-B1) - (A0-B0) ) / power
 
-def uniform_eq( halo ):
-	'''
-	Analytic function for a uniform distribution of dust particles 
-		from parameters set in halo (taux, a0, a1, p)
+def uniform_eq( halo, **kwargs ):
+    '''
+    Analytic function for a uniform distribution of dust particles 
+        from parameters set in halo (taux, a0, a1, p)
     --------------------------------------------------------------
-	FUNCTION uniform_eq( halo )
-	RETURNS : np.array [arcsec^-2] : I_h/F_a
-	Also alter halo.intensity with the resulting valuesa
+    FUNCTION uniform_eq( halo )
+    RETURNS : np.array [arcsec^-2] : I_h/F_a
+    Also alter halo.intensity with the resulting valuesa
     --------------------------------------------------------------
-	halo : halo.Halo object
-	'''
-	hfrac = halo.taux
-
-	if type(halo.rad) == dust.Grain:
-		charsig = 1.04 * 60. / halo.rad.a  / halo.energy  #arcsec
-		eterm  = 1 - erf( halo.alpha / charsig / np.sqrt(2.) )
-		result = hfrac * eterm * np.sqrt(np.pi/2.0) / (2.0*np.pi*charsig*halo.alpha)
-
-	if type(halo.rad) == dust.Dustdist:
-		charsig = 1.04 * 60.0 / halo.energy
-		const = hfrac / ( halo.alpha * charsig * np.sqrt(8.0*np.pi) )
-		result = const * G_u(halo) / G_p(halo)
-
-	halo.intensity = result
-	return result
-	
+    halo : halo.Halo object
+    '''
+    
+    set_htype( halo, xg=None, **kwargs )
+    
+    if type(halo) == Halo:
+        hfrac = halo.taux
+        energy, alpha = halo.energy, halo.alpha
+    
+    if type(halo) == HaloDict:
+        NE, NA = len(halo.energy), len(halo.alpha)
+        hfrac = np.tile( halo.taux.reshape(NE,1), NA ) # NE x NA
+        energy, alpha = halo.superE, halo.superA # NE x NA
+    
+    if type(halo.rad) == dust.Grain:
+        charsig = 1.04 * 60. / halo.rad.a  / energy  #arcsec
+        eterm  = 1 - erf( alpha / charsig / np.sqrt(2.) )
+        result = hfrac * eterm * np.sqrt(np.pi/2.0) / (2.0*np.pi*charsig*alpha)
+    
+    if type(halo.rad) == dust.Dustdist:
+        charsig = 1.04 * 60.0 / energy
+        const = hfrac / ( alpha * charsig * np.sqrt(8.0*np.pi) )
+        result = const * G_u(halo) / G_p(halo)
+    
+    halo.intensity = result
+    return
 
--- halodict.py
+++ halodict.py
@@ -16,8 +16,8 @@ from scipy.interpolate import interp1d
 ## See sim_cygx3.py for original code (and testing)
 
 #---------------------------------------------------------------
-# Supporting objects
 
+"""
 class Spectrum( object ):
     def __init__( self, bin, flux ):
         self.bin  = bin     # np.array
@@ -64,33 +64,32 @@ class Spectrum( object ):
         except:
             pass
         return
+"""
 
 class HaloDict( object ):
     """
     A dictionary of halos where each property can be looked up with energy as a key.
     """
-    def __init__( self, energy, alpha=np.arange(1.0,200.0,1.0), rad=dust.Grain(), scatm=ss.Scatmodel() ):
-        halo_list   = []
-        for ee in energy:
-            halo_list.append( gh.Halo( ee, alpha=alpha, rad=rad, scatm=scatm ) )
+
+    def __init__( self, energy, alpha=np.arange(1.0,200.0,1.0), \
+    	rad=dust.Grain(), scatm=ss.Scatmodel() ):
 
         self.alpha  = alpha
         self.energy = energy
-        self.dict   = dict( zip(energy,halo_list) )
+        self.index  = dict( zip(energy,range(len(energy))) )
         self.rad    = rad
         self.scatm  = scatm
         self.intensity = np.zeros( shape=( len(energy), len(alpha) ) )
+        
+		# The following variables get defined when htype is set
+		# See analytic.set_htype
+        self.htype  = None
+        self.dist   = None
+        self.taux   = None
 
-	@property
-	def superE( self ):
-		return np.tile( alpha, (NE,1) ) # NE x NA
-	
-	@property
-	def superA( self ):
-		return np.tile( energy.reshape(NE,1), NA ) # NE x NA
-
-    def __getitem__( self, key ):
-        return self.dict[key]
+	def __getitem__( self, key ):
+		i = self.index[key]
+		return self.intensity[i,:]
 
     ## http://stackoverflow.com/questions/19151/build-a-basic-python-iterator
     def __iter__( self ):
@@ -101,52 +100,47 @@ class HaloDict( object ):
             raise StopIteration
         else:
             self.count += 1
-            return self[ self.energy[self.count-1] ]
+            #return self[ self.energy[self.count-1] ]
+            return self.intensity[count,:]
 
     def __getslice__( self, i, j ):
-        slice  = np.where( np.logical_and( self.energy>=i, self.energy<j ) )[0]
-        result = HaloDict( self.energy[slice], alpha=self.alpha )
-        for ee in self.energy[slice]:
-            result.dict[ee] = self.dict[ee]
-        return result
-
-    def keys( self ):
-        return self.dict.keys()
-    def values( self ):
-        return self.dict.values()
-
-    @property
-    def taux( self ):
-    	return self[ self.energy[0] ].taux * self.energy[0]**2 / self.energy**2
-#        result = []
-#        for ee in self.energy:
-#            result.append( self.dict[ee].taux[0] )
-#        return np.array( result )
-
+    	slice  = np.where( np.logical_and( self.energy>=i, self.energy<j ) )[0]
+    	return self.intensity[slice,:]
+	
     @property
     def len( self ):
         return len( self.energy )
+
     @property
     def hsize( self ):
         return len( self.alpha )
 
+    @property
+    def superE( self ):
+        NE, NA = len(self.energy), len(self.alpha)
+        return np.tile( self.energy.reshape(NE,1), NA ) # NE x NA
+
+    @property
+    def superA( self ):
+        NE, NA = len(self.energy), len(self.alpha)
+        return np.tile( self.alpha, (NE,1) ) # NE x NA
+
     def total_halo( self, fluxes ):
         """
         Sums the halos from different energy bins to create a total halo profile.
+        Creates / updates the self.total parameter
         """
-        NE = len(self.energy)
+        NE, NA = len(self.energy), len(self.alpha)
         if len(fluxes) != NE:
             print 'Error: Number of flux bins must equal the number of halo energy bins'
             return
-
-        result = np.zeros( self.hsize )
-        for i in range(NE):
-            ee = self.energy[i]
-            result += fluxes[i] * self[ee].intensity
+        
+        superflux  = np.tile( fluxes.reshape(NE,1), NA )
+        result     = np.sum( superflux * self.intensity, 0 )
         self.total = result
-        self.bfrac = c.intz( self.alpha, result*2.0*np.pi*self.alpha ) \
-            / np.sum( fluxes )  # brightness fraction
+        return
 
+	# I want to rewrite this, but ignore for now
     def ecf( self, theta, nth=100 ):
         """
         Returns the enclosed fraction for the halo surface brightness
@@ -179,37 +173,13 @@ class HaloDict( object ):
 #---------------------------------------------------------------
 # Supporting functions
 
-def sum_spectra( spec1, spec2, emin, emax, dE, restore=False ):
-
-    spec1.rebin( emin, emax, dE )
-    spec2.rebin( emin, emax, dE )
-
-    sum_bin  = spec1.bin
-    sum_flux = spec1.flux + spec2.flux
-
-    if restore:
-        spec1.restore()
-        spec2.restore()
-
-    return Spectrum( sum_bin, sum_flux )
+def get_spectrum( filename ):
+	data = c.read_table( filename, 2 )
+	energy, flux = np.array( data[0] ), np.array( data[1] )
+	return energy, flux
 
 def aeff( filename ):
 	data = c.read_table( filename, 2 )
 	energy = np.array( data[0] )
 	aeff   = np.array( data[1] )
 	return interp1d( energy, aeff )   # keV vs cm^2
-
-def bin_halo( alpha, hbright, binedges ):
-    total  = []
-    area   = []
-    for i in range( len(binedges)-1 ):
-        area.append( np.pi * ( binedges[i+1]**2 - binedges[i]**2 ) )  # pixels^2 or arcsec^2
-        ia = np.where( np.logical_and( alpha>=binedges[i], alpha<binedges[i+1] ) )[0]
-        if len(ia) == 0:
-            total.append( 0 )
-        else:
-            total.append( c.intz( alpha[ia], hbright[ia] * 2*np.pi*alpha[ia] ) )
-    result = np.array(total) / np.array(area)
-    return result
-
-
