93dc1522cda5bfb5dcb8392acf0350e738ac8c69
Lia Corrales <lia@space.mit.edu>
2016-03-20 11:44:35 -0400

    Removed function calls. I'm going to have to fix this everywhere else

--- constants.py
+++ constants.py
@@ -48,61 +48,21 @@ omega_l = 0.7   # dark energy
 cperh0  = (clight * 1.e-5 / h0) * (1.e6 * pc2cm)
 
 
+##----- Very basic integration functions ------##
+## I think I used these for checking something
+## way back in the day
 
-def h0():
-    return 75.  #km/s/Mpc
-
-def rho_crit():
-    return np.float64(1.1e-29)
-
-def omega_d():
-    return 1e-5
-
-def omega_m():
-    return 0.3
-
-def omega_l():
-    return 0.7
-
+# Wrapper for scipy integration, used in cosmology integral
 def intz(x, y):
     from scipy import integrate
     return sp.integrate.trapz(y,x)
 # Note that scipy calls integration in reverse order as I do
 
-def int(x, y):
+# Basic trapezoidal integration function
+def trapezoidal_int(x, y):
     dx = x[1:] - x[:-1]
     dy = y[1:] - y[:-1]
     return np.sum( y[:-1]*dx + 0.5*dx*dy )
-
-def c():
-    return 3.e10 # cm/s
-
-def h():
-    return np.float64( 4.136e-18 )  # keV s
-
-def re():
-    return 2.83e-13  # cm (electron radius)
-
-def mp():
-    return np.float64( 1.673e-24 ) # g (mass of proton)
-
-def micron2cm():
-    return 1e-6 * 100
-
-def pc2cm():
-    return 3.09e18
-
-def cperh0():
-    return c()*1.e-5 / h0() * (1.e6 * pc2cm() )  #cm
-
-def kev2lam():
-    return 1.240e-7   # cm keV
-
-def arcs2rad():
-    return 2.0*np.pi / 360. / 60. / 60.  # rad/arcsec
-
-def arcm2rad():
-    return 2.0*np.pi / 360. / 60.        # rad/arcmin
 
 
 #------- Save and restore functions, similar to IDL -------#
