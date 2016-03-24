cd8bc463af83f7130b0a93a32b5616455a44933e
Lia Corrales <lia@space.mit.edu>
2016-03-24 19:30:15 -0400

    don't touch halodict or model_halo for now

--- halos/__init__.py
+++ halos/__init__.py
@@ -4,6 +4,6 @@ from .halo import Halo
 from . import cosmology
 from . import cosmhalo
 from . import galhalo
-from . import halodict
-from . import model_halo
+#from . import halodict
+#from . import model_halo
 from . import analytic
