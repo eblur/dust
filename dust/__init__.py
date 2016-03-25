fed1cac6cf009aca5e7060b3437fd82b9cd38d35
Lia Corrales <lia@space.mit.edu>
2016-03-24 22:31:54 -0400

    tests pass

--- dust/__init__.py
+++ dust/__init__.py
@@ -9,13 +9,12 @@ This is an Astropy affiliated package.
 from ._astropy_init import *
 # ----------------------------------------------------------------------------
 
-# For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
-    from example_mod import *
-
 # ----------------------------------------------------------------------------
 # eblur/dust specific imports
 
-from . import distlib
-from . import extinction
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    from . import distlib
+    from . import extinction
+
 # ----------------------------------------------------------------------------
