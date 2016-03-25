722ae5e204959f61cb78298d5a8addfba2e7951a
Lia Corrales <lia@space.mit.edu>
2016-03-24 22:28:59 -0400

    following astropy packages tutorial

--- dust/__init__.py
+++ dust/__init__.py
@@ -1,10 +1,21 @@
 
+"""
+This is an Astropy affiliated package.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    from example_mod import *
+
+# ----------------------------------------------------------------------------
+# eblur/dust specific imports
+
 from . import distlib
 from . import extinction
-
-
-#import constants as c
-#from cmlib import cmindex
-#from extinction import scatmodels
-#from extinction import sigma_scat
-#from composition import minerals
+# ----------------------------------------------------------------------------
