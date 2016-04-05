972d10a8d6b93e006475747746ebf6f8e23e0f55
Lia Corrales <lia@space.mit.edu>
2016-04-05 12:02:52 -0400

    streamlined a few more tests

--- astrodust/test_bhmie.py
+++ astrodust/test_bhmie.py
@@ -1,11 +1,22 @@
 """Test the bhmie module"""
 
+import pytest
+import numpy as np
+
 from astrodust.extinction.bhmie import *
 from astrodust.distlib.composition import cmindex as cmi
 
 A1 = 0.1  # um
 E1 = 1.0  # keV
-bhmobj = BHmie(A1, E1, cmi.CmGraphite())
+ALIST = np.linspace(0.05, 0.3, 10)
+ELIST = np.logspace(-1, 1, 20)
 
-#def test_one_grainsize():
-#
+@pytest.mark.parametrize('vals',[[A1, E1],[A1,ELIST],[ALIST,E1],[ALIST,ELIST]])
+def test_bhmie_instance(vals):
+    a, e = vals
+    bhobj = BHmie(a, e, cmi.CmSilicate())
+    assert bhobj.E.shape == bhobj.a.shape
+
+'''def test_one_grainsize():
+    bhobj = BHmie(A1, ELIST, cmi.CmSilicate())
+    bhobj.calculate()'''
