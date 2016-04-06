336255b5caf1bd5125d71bcb39d11c710e8d27bf
Lia Corrales <lia@space.mit.edu>
2016-04-06 11:44:45 -0400

    BHmie.calculate runs with an array of theta

--- astrodust/test_bhmie.py
+++ astrodust/test_bhmie.py
@@ -10,6 +10,7 @@ A1 = 0.1  # um
 E1 = 1.0  # keV
 ALIST = np.linspace(0.05, 0.3, 10)
 ELIST = np.logspace(-1, 1, 20)
+THLIST = np.logspace(-1, 2, 5)
 
 @pytest.mark.parametrize('vals',[[A1, E1],[A1,ELIST],[ALIST,E1],[ALIST,ELIST]])
 def test_bhmie_instance(vals):
@@ -20,7 +21,16 @@ def test_bhmie_instance(vals):
 @pytest.mark.parametrize('vals',[[A1, E1],[A1,ELIST],[ALIST,E1],[ALIST,ELIST]])
 def test_BHmie_calculate(vals):
     a, e = vals
-    print("NA = %d, NE = %d, NTH = 1" % (np.size(a), np.size(e)))
+    #print("NA = %d, NE = %d, NTH = 1" % (np.size(a), np.size(e)))
     bhobj = BHmie(a, e, cmi.CmSilicate())
     bhobj.calculate()
     assert bhobj.qsca.shape == bhobj.X.shape
+    assert bhobj.S1.shape[1:] == (bhobj.NA, bhobj.NE, bhobj.NTH)
+
+@pytest.mark.parametrize('vals',[[A1, E1],[A1,ELIST],[ALIST,E1],[ALIST,ELIST]])
+def test_BHmie_calculate_theta(vals):
+    a, e  = vals
+    bhobj = BHmie(a, e, cmi.CmSilicate())
+    bhobj.calculate(THLIST)
+    assert bhobj.qsca.shape == bhobj.X.shape
+    assert bhobj.S1.shape[1:] == (bhobj.NA, bhobj.NE, bhobj.NTH)
